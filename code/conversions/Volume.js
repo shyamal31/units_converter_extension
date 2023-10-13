1ef216369cbbf0e87f740be8876ba34b2c503861
Ankit Garg <41066963+ankitgargsma@users.noreply.github.com>
2023-10-13 18:15:09 -0400

    Volume.js
    Adding a new conversion unit file, "Volume.js". This will handle the unit conversion for various volume units as a part of chrome extension.
    
    Signed-off-by: Ankit Garg <41066963+ankitgargsma@users.noreply.github.com>

---
+++ code/conversions/Volume.js
@@ -0,0 +1,134 @@
+//Our Volume class handles all Volume related conversions
+/**
+ * Volumes class handles all Volume related conversions
+ */
+class Volume {
+    /**
+     * @constructor
+     * @param {Object} unit data unit
+     * @param {Array} arr conversion rates array
+     */
+    constructor(unit, arr) {
+        this.unit = unit;
+        this.arr = arr;
+    }
+
+    /**
+     * 
+     * @param {Number} number input number value
+     * @returns returns value with accuracy of input number decimal points 
+     */
+    getPrecision(number) {
+        let parts = number.toString().split(".");
+        if (parts.length <= 1) {
+            return number < 0 ? parts[0].length - 1 : parts[0].length;
+        }
+        let intlen = number < 0 ? parts[0].length - 1 : parts[0].length;
+        return intlen + parts[1].length;
+    }
+
+    /** 
+     * @param {Number} number input number value
+     * @param {Number} precision input precision digit number
+     * @returns number after precision
+     */
+    getPreciseNumber(number, precision) {
+        return number.toPrecision(
+            Math.min(Math.max(this.getPrecision(number), precision), 10)
+        );
+    }
+
+
+
+    /**
+     * Our standard conversion is Litres, so we try to convert all selection to Litres
+     * @param {Number} quantity input number
+     * @returns returns value with accuracy of 10 decimal points 
+     */
+
+    getStandardConversion(quantity) {
+        //We use a switch case to make appropriate conversion
+        switch (this.unit.toLowerCase()) {
+            case "litres":
+                return quantity;
+            case "milliliter":
+                return quantity / 1000;
+            case "cubic meter":
+                return quantity / 0.001;
+            case "cubic inch":
+                return quantity / 61.023744;
+            case "cubic foot":
+                return quantity / 0.035314666;
+            case "pint":
+                return quantity / 2.1133764188;
+            case "quart":
+                return quantity / 1.056688209;
+            case "gallon":
+                return quantity / 0.26417205235815;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * From our standard conversion we try to convert into all the other units specified in arr property of this class with a precision no more than 10
+     * @param {Number} quantity input quantity number
+     * @param {Number} precision input precision digit
+     * @returns all values with accuracy of 10 decimal points 
+     */
+    /**/
+    getAllConversions(quantity, precision) {
+
+        let res = ""; 
+        this.arr.forEach((u) => {
+            switch (u.toLowerCase()) {
+                case "litres":
+                    res += "," + this.getPreciseNumber(quantity, precision) + " lt";
+                    break;
+                case "milliliter": {
+                    let conv = quantity * 1000;
+                    res += "," + this.getPreciseNumber(conv, precision) + " ml";
+                    break;
+                }
+                case "cubic meter": {
+                    let conv = quantity * 0.001;
+                    res += "," + this.getPreciseNumber(conv, precision) + " m3";
+                    break;
+                }
+                case "cubic inch": {
+                    let conv = quantity * 61.023744;
+                    res += "," + this.getPreciseNumber(conv, precision) + " in3";
+                    break;
+                }
+                case "cubic foot": {
+                    let conv = quantity * 0.035314666;
+                    res += "," + this.getPreciseNumber(conv, precision) + " ft3";
+                    break;
+                }
+                case "pint": {
+                    let conv = quantity * 2.113376418;
+                    res += "," + this.getPreciseNumber(conv, precision) + " pint";
+                    break;
+                }
+                case "quart": {
+                    let conv = quantity * 1.056688209;
+                    res += "," + this.getPreciseNumber(conv, precision) + " qt";
+                    break;
+                }
+                case "gallon": {
+                    let conv =  quantity * 0.26417205235815;
+                    res += "," + this.getPreciseNumber(conv, precision) + " gal";
+                    break;
+                }
+            }
+        });
+
+        return res;
+    }
+
+
+}
+
+if(typeof module == 'object') {
+	module.exports = Volume;
+}
