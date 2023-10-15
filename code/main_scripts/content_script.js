0aab6b71de0628f2b75979f966e1e033df80979d
Nihit Mittal <nihitmittal@Nihits-MBP.lan>
2023-10-15 09:55:42 -0400

    Added the button

--- code/main_scripts/content_script.js
+++ code/main_scripts/content_script.js
@@ -15,14 +15,83 @@ document.addEventListener('mouseup', (e) => {
         // Get all possible unit conversions for given selections
         (async () => {
             let result = await get_conversions(selection);
-            if (result.length > 0) {
-                // Once the result is obtained, give the modal a heading as the trimmed selection and add each comma seperated results to new line
-                result = "<p class=\"modal_heading\">" + selection + "</p>" +
-                    "<p class=\"modal_content\">" +
-                    result.replace(/,/g, "<hr class=\"modal_newline\">") + "</p>";
-                // Display  the modal
-                showModal(e.clientX, e.clientY, result);
+            // Provided data
+           // var data = "0.03809997943 m,0.00002367929113 mi,0.00003809997943 km,3.809997943 cm,38.09997943 mm,0.04166651850 yd,0.1249999365 ft,0.0001895521364 fur,0.001893919542 chain";
+
+            // Split the data into an array based on the comma
+            var dataArray = result.result.split(',');
+
+            // Initialize two arrays for data greater and less than 10^-3
+            var greaterThan10Minus3 = [];
+            var lessThan10Minus3 = [];
+
+            // Traverse the data and segregate it
+            for (var i = 0; i < dataArray.length; i++) {
+                var value = parseFloat(dataArray[i].split(' ')[0]); // Parse the numeric value
+                var unit = dataArray[i].split(' ')[1]; // Extract the unit
+
+                if (value > 0.001 && value < 1000) {
+                    // Data greater than 10^-3 and less than 10^3
+                    greaterThan10Minus3.push(value + ' ' + unit);
+                } else {
+                    // Data outside the specified range
+                    lessThan10Minus3.push(value + ' ' + unit);
+                }
             }
+
+            // Output the segregated data
+            console.log("Data greater than 10^-3 and less than 10^3:");
+            console.log(greaterThan10Minus3);
+
+            console.log("Data outside the specified range:");
+            console.log(lessThan10Minus3);
+            //console.log("Result from content script:- ",result);
+            
+            // Assuming you have the data segregation code from the previous response
+
+            // Create a function to update the modal content
+            function updateModalContent() {
+                // Combine the content for the "View All" button
+                var content = greaterThan10Minus3;
+
+                // Create the content for the "View All" button
+                var viewAllButton = "<button id=\"viewAllButton\">View All</button>";
+
+                // Add the "View All" button if applicable
+                if (lessThan10Minus3.length > 0) {
+                    content.push(viewAllButton);
+                }
+
+                // Display the modal
+                var resultContent = "<p class=\"modal_heading\">" + selection + "</p>" +
+                    "<p class=\"modal_content\">" + content.join("<hr class=\"modal_newline\">") + "</p>";
+                showModal(e.clientX, e.clientY, resultContent);
+
+                // Add an event listener to the "View All" button
+                var viewAllButtonElement = document.getElementById("viewAllButton");
+                if (viewAllButtonElement) {
+                    viewAllButtonElement.addEventListener("click", function() {
+                        // Append the content from the lessThan10Minus3 array
+                        var contentForLess = lessThan10Minus3.join("<hr class=\"modal_newline\">");
+                        var extendedContent = "<p class=\"modal_heading\">" + selection + "</p>" +
+                            "<p class=\"modal_content\">" + greaterThan10Minus3.join("<hr class=\"modal_newline\">") +
+                            "<hr class=\"modal_newline\">" + contentForLess + "</p>";
+                        showModal(e.clientX, e.clientY, extendedContent);
+                    });
+                }
+            }
+
+            // Initial display
+            updateModalContent();
+
+            // if (result.result.length > 0) {
+            //     // Once the result is obtained, give the modal a heading as the trimmed selection and add each comma seperated results to new line
+            //     result.result = "<p class=\"modal_heading\">" + selection + "</p>" +
+            //         "<p class=\"modal_content\">" +
+            //         result.result.replace(/,/g, "<hr class=\"modal_newline\">") + "</p>";
+            //     // Display  the modal
+            //     showModal(e.clientX, e.clientY, result.result);
+            // }
         })();
     }
 
