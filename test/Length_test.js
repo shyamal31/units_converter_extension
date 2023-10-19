e699a1a1574a114aa7a939fc9017f7ef2173606d
Nihit Mittal <nihitmittal@Nihits-MBP.lan>
2023-10-19 04:43:01 -0400

    Matched the output for the newly introduces feature

--- test/Length_test.js
+++ test/Length_test.js
@@ -55,7 +55,7 @@ describe('Length', function() {
   describe('#getAllConversions(quantity, precision)', function() {
     it('should return ', function() {
       const length = new Length("kilometers", ["kilometers", "meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
-      assert.strictEqual(length.getAllConversions(3, 2), ',0.003000 km,3.0 m,0.001864512119 mi,300 cm,3000 mm,3.28083 yd,9.84252 ft,118.1103 inch,0.01492537313 fur,0.1491276035 chain')
+      assert.strictEqual(length.getAllConversions(3, 2), ',0.003000 km,3 m,0.001864512119 mi,300 cm,3000 mm,3.281 yd,9.843 ft,118.11 inch,0.015 fur,0.149 chain')
     })
 
     it('should return ', function() {
