f230f9fae349aa29590bf9fade55b362c6b19ac2
Nihit Mittal <nihitmittal@Nihits-MBP.lan>
2023-10-19 04:48:38 -0400

    Removed the redundant code

--- code/conversions/Data.js
+++ code/conversions/Data.js
@@ -17,9 +17,6 @@ class Data {
    */
   getPrecision(number) {
     let parts = number.toString().split(".");
-    if (parts.length <= 1) {
-      return number < 0 ? parts[0].length - 1 : parts[0].length;
-    }
     let intlen = number < 0 ? parts[0].length - 1 : parts[0].length;
     return intlen + parts[1].length;
   }
