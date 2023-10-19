cacdf009fe6bf85621dbba39fe8b0e17430deab8
Ankit Garg <ankitgargsma@gmail.com>
2023-10-18 21:05:38 -0400

    Added the naming coventions for fluid oz

--- code/main_scripts/units_objects.js
+++ code/main_scripts/units_objects.js
@@ -350,6 +350,6 @@ const UNITS = [
   {
     unit: "fl oz",
     type: "volume",
-    aliases: ["fluid ounce", " fluid ounces", "FlOz", "Oz", "Ounces", "Ounce" , "US fluid ounce", "US fl oz"],
+    aliases: ["fluid ounce", " fluid ounces", "FlOz", "Oz", "Ounces", "Ounce" , "US fluid ounce", "US fl oz", "Fl Oz"],
   },
 ];
