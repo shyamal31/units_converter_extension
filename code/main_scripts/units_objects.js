d01535f665e612558aad95a6d13619fedfbfce1c
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2023-11-21 16:27:51 -0500

    add ratio to length units

--- code/main_scripts/units_objects.js
+++ code/main_scripts/units_objects.js
@@ -121,121 +121,145 @@ const UNITS = [
     unit: "meters",
     type: "length",
     aliases: ["meter", "meters", "m", "mts"],
+    ratio: 1
   },
   {
     unit: "kilometers",
     type: "length",
     aliases: ["kilometer", "kilometers", "km", "kms"],
+    ratio: .001
   },
   {
     unit: "miles",
     type: "length",
     aliases: ["miles", "mile", "mi"],
+    ratio: 0.000621371
   },
   {
     unit: "centimeters",
     type: "length",
     aliases: ["centimeter", "centimeters", "cm", "cms"],
+    ratio: 100
   },
   {
     unit: "millimeters",
     type: "length",
     aliases: ["millimeter", "millimeters", "mm", "mms"],
+    ratio: 1000
   },
   {
     unit: "yard",
     type: "length",
     aliases: ["yard", "yards", "yd", "yds"],
+    ratio: 1.09361
   },
   {
     unit: "feet",
     type: "length",
     aliases: ["foot", "feet", "ft", "fts"],
+    ratio: 3.28084
   },
   {
     unit: "inch",
     type: "length",
     aliases: ["inches", "inch", '"'],
+    ratio: 39.3701
   },
   {
     unit: "furlong",
     type: "length",
     aliases: ["furlong", "furlongs", "fur", "furss"],
+    ratio: 0.00497096
   },
   {
     unit: "chain",
     type: "length",
     aliases: ["chain", "chains"],
+    ratio: 0.0497097
   },
   {
     unit: "seconds",
     type: "time",
     aliases: ["sec", "s", "secs", "second", "seconds"],
+    ratio: 604800
   },
   {
     unit: "minutes",
     type: "time",
     aliases: ["mins", "min", "minute", "minutes"],
+    ratio: 10080
   },
   {
     unit: "hours",
     type: "time",
     aliases: ["hrs", "h", "hour", "hours"],
+    ratio: 168
   },
   {
     unit: "days",
     type: "time",
     aliases: ["day", "days"],
+    ratio: 7
   },
   {
     unit: "weeks",
     type: "time",
     aliases: ["week", "wks", "weeks"],
+    ratio: 1
   },
   {
     unit: "m2",
     type: "area",
     aliases: ["m2", " m2", "m²", "sqm"],
+    ratio: 1
   },
   {
     unit: "cm2",
     type: "area",
     aliases: ["cm2", " cm2", "cm²", "sqcm"],
+    ratio: 10000
   },
   {
     unit: "km2",
     type: "area",
     aliases: ["km2", " km2", "km²", "sqkm"],
+    ratio: 0.000001
   },
   {
     unit: "miles2",
     type: "area",
     aliases: ["miles2", " miles2", "miles²", "sqmiles"],
+    ratio: 0.0000003861
   },
   {
     unit: "ft2",
     type: "area",
     aliases: ["ft2", " ft2", "ft²", " sqft"],
+    ratio: 10.76385024645
   },
   {
     unit: "in2",
     type: "area",
     aliases: ["in2", " in2", "in²", "sqin"],
+    ratio: 1549.9944354888
   },
   {
     unit: "ha2",
     type: "area",
     aliases: ["ha2", " ha2", "ha²", "sqha"],
+    ratio: 0.0001
   },
   {
     unit: "acre2",
     type: "area",
     aliases: ["acre2", " acre2", "acre²", "sqacre"],
+    ratio: 0.000247105
   },
   {
     unit: "yd2",
     type: "area",
     aliases: ["yd2", " yd2", "yd²", "sqyd"],
+    ratio: 1.19599
   },
   {
     unit: "b/s",
